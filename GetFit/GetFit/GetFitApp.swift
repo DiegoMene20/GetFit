96a3a0611b1dbbf173fed189a30487d2c01ec563
Diego Menegatti <ltdiegosc@gmail.com>
2023-03-29 23:25:24 -0400

    first commit

---
+++ GetFit/GetFit/GetFitApp.swift
@@ -0,0 +1,32 @@
+//
+//  GetFitApp.swift
+//  GetFit
+//
+//  Created by Diego Menegatti on 3/29/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
+@main
+struct GetFitApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
